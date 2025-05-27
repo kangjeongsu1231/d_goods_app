e4a0972238310681f845ba43f5ddca36b0702916
kangjeoungsu <asasas1231@naver.com>
2025-05-28 00:14:20 +0900

    ios 설정

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();  // 여기 꼭 추가!
+  // await Firebase.initializeApp();  // 여기 꼭 추가!
   runApp(const MyApp());
 }
 
@@ -16,24 +16,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: SvgPicture.asset(
-            'assets/images/your_image.svg',
-          )
+          title: Text('로고 없는 테스트'),
+          centerTitle: true,
         ),
-        body: ShopItem(),
-
+        body: Center(child: Text('Hello Flutter')),
       )
-    );
-  }
-}
-
-class ShopItem extends StatelessWidget {
-  const ShopItem({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      child: Text('안녕'),
     );
   }
 }
